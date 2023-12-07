1d8486322e15f9c786339ab2aa0b38ffc95fef27
Eduardo Puga <0470hdbutter@gmail.com>
2023-12-06 16:56:17 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/sad-robot.jpg" alt="Sad Robot" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@mullyadii?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mulyadi</a> on <a href="https://unsplash.com/photos/grayscale-photo-of-box-robot-on-table-ZnLprInKM7s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@cravethebenefits?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Brenda Godinez</a> on <a href="https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
