4df099d9539ddb2201649723d5c1d57cd4edc90f
Eduardo Puga <0470hdbutter@gmail.com>
2023-12-08 19:11:48 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,9 +5,21 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-
 router.get('/new', (req, res) => {
   res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show' , { place: places[id] } )
+  }
 })
 
 
